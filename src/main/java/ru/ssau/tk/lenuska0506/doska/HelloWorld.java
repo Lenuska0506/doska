e6713e10c0086434b173cbfd64455af57557c9be
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-09-13 20:41:30 +0400

    pom.xml

--- src/main/java/ru/ssau/tk/lenuska0506/doska/HelloWorld.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/HelloWorld.java
@@ -3,7 +3,6 @@ package ru.ssau.tk.lenuska0506.doska;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello, World!");
     }
 
 }
