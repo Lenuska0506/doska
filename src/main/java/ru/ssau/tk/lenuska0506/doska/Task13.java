279c59f5623450d0f3537ed10cef3aacedfd0b42
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-09-15 19:53:40 +0400

    выполнила задание 1.3 в файле Task11.java

---
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task13.java
@@ -0,0 +1,23 @@
+package ru.ssau.tk.lenuska0506.doska;public class Task13 {
+}
+
+class Main1 {
+    public static void main(String[] s) {
+        Point o = new Point(1,4,5);
+        Point o1 = new Point(2,3,4);
+    }
+};
+
+class Point {
+    public  final double x;
+    public  final double y;
+    public  final double z;
+
+    public Point (double x1,double y1,double z1) {
+        x = x1;
+        y = y1;
+        z = z1;
+    }
+
+
+}
