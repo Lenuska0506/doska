83b0f673ee0db958ada33fd31b44cd16d1f969b6
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-09-27 00:58:40 +0400

    заданиче 14.1)статический метод checkAnotherPerson- выводит игнат,так как копии ссылки присваивается новое значениее,тое есть теперь у нас новая ссылка2)оригинальная ссылка при этом никак не зменилась и осталась акадием

--- src/main/java/ru/ssau/tk/lenuska0506/doska/Task133.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task133.java
@@ -1,26 +1,15 @@
 package ru.ssau.tk.lenuska0506.doska;
 class coc {
-
-
     public static void main(String[] args) {
         Person person = new Person();
         person.setName("Arkadiy");
-        int number = 5;
-        checkInt(number);
-        checkPerson(person);
-
-        System.out.println(number);
+        checkAnotherPerson(person);
+        System.out.println(person.getName());
+    }
+    private static void checkAnotherPerson(Person person) {
+        person = new Person();
+        person.setName("Ignat");
         System.out.println(person.getName());
     }
 
-    private static void checkInt(int number) {
-        number = 10;
-        System.out.println(number);
-    }
-
-    private static void checkPerson(Person person) {
-
-        person.setName("Oleg");
-        System.out.println(person.getName());
-    }
 }
