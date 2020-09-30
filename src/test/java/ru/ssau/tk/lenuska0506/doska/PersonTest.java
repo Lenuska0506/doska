9c54683c150a9291cd5831154f82c9fdfc55aea6
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-01 01:43:32 +0400

    обновила тесты класса Person

--- src/test/java/ru/ssau/tk/lenuska0506/doska/PersonTest.java
+++ src/test/java/ru/ssau/tk/lenuska0506/doska/PersonTest.java
@@ -13,17 +13,20 @@ public class PersonTest {
         o1.setFirstName("Samoletik");
         o1.setLastName("Gosha");
         o1.setPassportId(7654321);
+        o.setGender(Gender.FEMALE);
 
         o.setLastName("Tank");
         o.setFirstName("Lena");
         o.setPassportId(1234567);
+        o1.setGender(Gender.MALE);
 
         assertEquals(o1.getFirstName(), "Samoletik");
         assertEquals(o1.getLastName(), "Gosha");
         assertEquals(o1.getPassportId(), 7654321);
-
+        assertEquals(o1.getGender(), Gender.MALE);
         assertEquals(o.getLastName(), "Tank");
         assertEquals(o.getFirstName(), "Lena");
         assertEquals(o.getPassportId(), 1234567);
+        assertEquals(o.getGender(), Gender.FEMALE);
     }
 }
