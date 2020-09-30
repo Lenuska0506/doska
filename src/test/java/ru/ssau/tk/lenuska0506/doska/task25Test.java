53f429ae5e05ad39b590ebadc4cd211a822dab68
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-01 00:59:57 +0400

    тесты к заданию 2.6

--- src/test/java/ru/ssau/tk/lenuska0506/doska/task25Test.java
+++ src/test/java/ru/ssau/tk/lenuska0506/doska/task25Test.java
@@ -4,3 +4,15 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
+public class task25Test {
+    int[] array = new task25().setNumber(8);
+
+    @Test
+    public void testSetNumber() {
+        assertEquals(array.length, 8);
+        assertEquals(array[0], 0);
+        assertEquals(array[1], 1);
+        assertEquals(array[2], 4);
+        assertEquals(array[3], 9);
+    }
+}
