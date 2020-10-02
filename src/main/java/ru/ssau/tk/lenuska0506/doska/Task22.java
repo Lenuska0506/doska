68627ed55ec1e99169091bf03ecbf96e8bc8a7ee
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-02 20:55:13 +0400

    задание 2.8

--- src/main/java/ru/ssau/tk/lenuska0506/doska/Task22.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task22.java
@@ -11,4 +11,18 @@ public class Task22 {
         return array;
     }
 }
+class Taks28 {
+    static double[] setNumber(int size) {
+        double[] arr = new double[size];
+        int j = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (j % 3 == 0) {
+                j++;
+            }
+            arr[i] = j;
+            j++;
+        }
+        return arr;
+    }
+}
 
