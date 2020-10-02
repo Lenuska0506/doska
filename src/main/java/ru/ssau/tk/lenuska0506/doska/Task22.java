24b768d7b1962710bbbb72a3366b5d30ac25967b
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-02 21:00:51 +0400

    задание 2.9

--- src/main/java/ru/ssau/tk/lenuska0506/doska/Task22.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task22.java
@@ -25,4 +25,14 @@ class Taks28 {
         return arr;
     }
 }
+class Task29 {
+    public static int[] setprogressia(int N, int d, int size) {
+        int[] arr = new int[size];
+        arr[0] = N;
+        for (int i = 1; i < arr.length; i++) {
+            arr[i] = arr[i - 1] + d;
+        }
+        return arr;
+    }
+}
 
