a54b40a7b6b4f76fbbf0f71418a9131a61fd536d
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-04 21:59:48 +0400

    задание 2.1 тест

--- src/test/java/ru/ssau/tk/lenuska0506/doska/task21Test.java
+++ src/test/java/ru/ssau/tk/lenuska0506/doska/task21Test.java
@@ -4,3 +4,15 @@ import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
 
+public class task21Test {
+    int[] arr = new Task21().setNumber(6);
+
+    @Test
+    public void testSetNumber() {
+        assertEquals(arr.length, 6);
+        assertEquals(arr[0], 0);
+        assertEquals(arr[2], 2);
+        assertEquals(arr[3], 3);
+
+    }
+}
