648ebe7e8f8862371d68e3f5664d39a2291edf0d
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-06 23:37:02 +0400

    задание 2.16

--- src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
@@ -1,9 +1,9 @@
 package ru.ssau.tk.lenuska0506.doska;
 
 public class Task215 {
-         static boolean setArr(int[] arr, int n) {
-            for (int j : arr) {
-                if (j == n) {
+         static boolean setArr(Integer[]  arr) {
+            for (int n : arr) {
+                if (arr[n] == null) {
                     return true;
                 }
             }
