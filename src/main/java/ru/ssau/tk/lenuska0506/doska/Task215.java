dc78d4d461da943eb05561d7549c7972fc32d5a5
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-06 23:29:35 +0400

    задание 2.15

---
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
@@ -0,0 +1,12 @@
+package ru.ssau.tk.lenuska0506.doska;
+
+public class Task215 {
+         boolean setArr(int[] arr, int n) {
+            for (int j : arr) {
+                if (j == n) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
