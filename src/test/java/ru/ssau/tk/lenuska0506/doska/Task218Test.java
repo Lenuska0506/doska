3f226a2fcfc9ebc275abe6ee9beb406dbf00a02a
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-10 22:57:23 +0400

    задание 2.18 тест

---
+++ src/test/java/ru/ssau/tk/lenuska0506/doska/Task218Test.java
@@ -0,0 +1,17 @@
+package ru.ssau.tk.lenuska0506.doska;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+public class Task218Test {
+
+    @Test
+    public void testMaxvalue() {
+        int[] array = new int[]{1, 3, 5};
+
+        assertNull(Task218.Maxvalue(new int[]{}),null);
+    }
+
+
+
+}
