5eb0ac5819aa017b875647c3762d96e977a0085c
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-15 23:34:29 +0400

    задание 2.23

--- src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
@@ -70,3 +70,26 @@ class Task217 {
         return k;
     }
 }
+
+ class Task223 {
+    static void setArr(int[] arr) {
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            max = arr[i] > max ? arr[i] : max;
+            min = arr[i] < min ? arr[i] : min;
+        }
+        int indexmin = 0;
+        int indexmax = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == min) {
+                indexmin = i;
+            }
+            if (arr[i] == max) {
+                indexmax = i;
+            }
+        }
+        arr[indexmin] = max;
+        arr[indexmax] = min;
+    }
+}
