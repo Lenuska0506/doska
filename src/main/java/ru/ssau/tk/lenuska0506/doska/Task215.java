f91d4019a45720326f641f63512224bd11f5f3df
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-17 23:33:23 +0400

    задание 2.26

--- src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
@@ -129,3 +129,12 @@ class Task217 {
         }
     }
 }
+ class Task226 {
+    static int[] getSumParn(int[] arr) {
+        int[] arr1 = new int[arr.length / 2 + (arr.length % 2 == 0 ? 0 : 1)];
+        for (int i = 0; i < arr1.length; i++) {
+            arr1[i] = arr[2 * i] + (2 * i + 1 < arr.length ? arr[2 * i + 1] : 0);
+        }
+        return arr1;
+    }
+}
