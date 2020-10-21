77390033d68121727d69c0a97b960879736044fa
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-21 22:34:49 +0400

    задание 1.19 добавила кусок с "null"

--- src/main/java/ru/ssau/tk/lenuska0506/doska/printType.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/printType.java
@@ -2,38 +2,46 @@ package ru.ssau.tk.lenuska0506.doska;
 
 public class printType {
 
-        public static void printType(byte a ) {
-            System.out.println("byte");
+    public static void printType(byte a) {
+        System.out.println("byte");
+    }
+
+    public static void printType(char a) {
+        System.out.println("char");
+    }
+
+    public static void printType(short a) {
+        System.out.println("short");
+    }
+
+    public static void printType(int a) {
+        System.out.println("int");
+    }
+
+    public static void printType(long a) {
+        System.out.println("long");
+    }
+
+    public static void printType(float a) {
+        System.out.println("float");
+    }
+
+    public static void printType(double a) {
+        System.out.println("double");
+    }
+
+    public static void printType(boolean a) {
+        System.out.println("boolean");
+    }
+
+    public static void printType(Object temp) {
+        if (temp == null) {
+            System.out.println("null");
+        } else {
+            System.out.println(temp.getClass());
         }
 
-        public static void printType(char a) {
-            System.out.println("char");
-        }
-
-        public static void printType(short a) {
-            System.out.println("short");
-        }
-
-        public static void printType(int a) {
-            System.out.println("int");
-        }
-
-        public static void printType(long a) {
-            System.out.println("long");
-        }
-
-        public static void printType(float a) {
-            System.out.println("float");
-        }
-
-        public static void printType(double a) {
-            System.out.println("double");
-        }
-
-        public static void printType(boolean a) {
-            System.out.println("boolean");
-        }
-
-        }
+    }
+}
 
     
