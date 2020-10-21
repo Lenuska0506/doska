a31ca443dc10d4d3cec583945e79101a634822e0
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-21 23:37:57 +0400

    задание 2.5

--- src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
@@ -151,3 +151,14 @@ class Task217 {
         return arr;
     }
 }
+ class Task25 {
+    public static int[] setNumber(int n) {
+        int[] arr = new int[n];
+        arr[0] = 1;
+        arr[1] = 1;
+        for (int i = 2; i < arr.length; i++) {
+            arr[i] = arr[i-1]+arr[i-2];
+        }
+        return arr;
+    }
+}
