177fbaa80ad6622c97af1b28c135e52c840d55a5
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-22 19:21:36 +0400

    задание 2.11

--- src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
@@ -162,3 +162,23 @@ class Task217 {
         return arr;
     }
 }
+ class Task211 {
+    static int[] getdelitei(int n) {
+        int c = 0;
+        for (int i = 1; i < Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                c++;
+            }
+        }
+        int[] arr = new int[c * 2 + (Math.sqrt(n) == (int) Math.sqrt(n) ? 1 : 0)];
+        int j = 0;
+        for (int i = 0; i < (int) Math.sqrt(n); i++) {
+            if (n % (i + 1) == 0) {
+                arr[j] = (i + 1);
+                arr[arr.length - j - 1] = n/ (i + 1);
+                j++;
+            }
+        }
+        return arr;
+    }
+}
