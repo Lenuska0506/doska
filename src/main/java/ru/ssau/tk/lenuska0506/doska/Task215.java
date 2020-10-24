5fde65b762f9187679ec880f1082c0746227f15e
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-24 23:26:41 +0400

    задание 2.12

--- src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
@@ -182,3 +182,23 @@ class Task217 {
         return arr;
     }
 }
+ class Task2_12 {
+    static double[] getDividersOfNumbers(int n) {
+        int count = 0;
+        for (int i = 1; i < Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+        double[] arr = new double[count * 2 + (Math.sqrt(n) == (int) Math.sqrt(n) ? 1 : 0)];
+        int j = 0;
+        for (int i = 0; i < (int) Math.sqrt(n); i++) {
+            if (n % (i + 1) == 0) {
+                arr[j] = (i + 1);
+                arr[arr.length - j - 1] = n / (i + 1);
+                j++;
+            }
+        }
+        return arr;
+    }
+}
