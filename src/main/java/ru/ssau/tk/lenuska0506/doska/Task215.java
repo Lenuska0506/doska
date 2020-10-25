7177cf7f2c29ab8258dc445772cb5b3b2744527f
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-10-25 22:10:01 +0400

    задание 3.3

--- src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task215.java
@@ -217,3 +217,19 @@ class Task32 {
         }
     }
 }
+ class Task33 {
+    public static void main(String[] args) {
+        String str = "wow";
+        String str1 = new String(str);
+        String str2 = "wow1";
+        if(str==str2){
+            System.out.println("str==str2");
+        }
+        if (str == str1) {
+            System.out.println("str==str1");
+        }
+        if (str.equals(str1)) {
+            System.out.println("str.equals(str1)");
+        }
+    }
+}
