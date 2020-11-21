6d55cf3dfb2963f18305918a11cc38ba32b3abd4
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-11-21 08:58:08 +0400

    задание 3.13

--- src/main/java/ru/ssau/tk/lenuska0506/doska/Task311.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task311.java
@@ -22,3 +22,17 @@ class Task312 {
         return _1str;
     }
 }
+ class Task313 {
+    static String getStringOfIndex(String str, int from, int to) {
+        if (from < 0) {
+            from = 0;
+        }
+        if (to > str.length()) {
+            to = str.length();
+        }
+        if (from == to) {
+            return "";
+        }
+        return str.substring(from, to);
+    }
+}
