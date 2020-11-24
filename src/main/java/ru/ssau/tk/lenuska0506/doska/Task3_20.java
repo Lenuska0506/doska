2f692393b84f0491fdc121256b67977426cb3b9d
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-11-24 22:43:09 +0400

    задание 3.5

--- src/main/java/ru/ssau/tk/lenuska0506/doska/Task3_20.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task3_20.java
@@ -1,5 +1,7 @@
 package ru.ssau.tk.lenuska0506.doska;
 
+import java.util.Objects;
+
 public class Task3_20 {
 
        static String getUnifiedStr(String[] array) {
@@ -9,6 +11,13 @@ public class Task3_20 {
  class Task3_4 {
     static boolean palindrom(String str){
         return str.equals(new StringBuilder(str).reverse().toString());
+    }
+}
+ class Task3_5 {
+    static boolean differentStr(String str1,String str2){
+        return !Objects.equals(null, str1) && !Objects.equals(null, str2)
+                && !str1.equals(str2)
+                && str1.equalsIgnoreCase(str2);
     }
 }
 
