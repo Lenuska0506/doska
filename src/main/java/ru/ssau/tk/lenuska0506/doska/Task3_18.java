3056f83bbb0eb2ee6e1ff447bd51cd6b8158d8ee
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-12-01 22:54:42 +0400

    задание 3.18

---
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task3_18.java
@@ -0,0 +1,9 @@
+package ru.ssau.tk.lenuska0506.doska;
+
+public class Task3_18  {
+    public static void getDescription(Object obj){
+        System.out.println("Описание объекта: ");
+        System.out.println(obj);
+    }
+}
+
