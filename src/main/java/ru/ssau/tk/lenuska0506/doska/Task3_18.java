d93aedfe7408ac48a499a46b4f5dcec83f5d08d9
lenuska0506 <smolnikova.lenuska@yandex.ru>
2020-12-06 22:21:53 +0400

    2.7

--- src/main/java/ru/ssau/tk/lenuska0506/doska/Task3_18.java
+++ src/main/java/ru/ssau/tk/lenuska0506/doska/Task3_18.java
@@ -24,3 +24,27 @@ public class Task3_18  {
                  .toArray(String[]::new);
      }
  }
+ class Task2_7 {
+    public static double[] setkdadratnoeyravnenie(double a, double b, double c) throws Exception {
+        if (a == 0) {
+            if (b == 0) {
+                if (c != 0) {
+                    throw new Exception("нет  корней");
+                } else {
+                    throw new Exception("комплексные корни ");
+                }
+            } else {
+                return new double[]{-c / b};// bx+c = 0
+            }
+        } else {
+            double d = b * b - 4 * a * c;
+            if (d > 0) {
+                return new double[]{(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)};
+            } else if (d == 0) {
+                return new double[]{-b / (2 * a)};
+            } else {
+                throw new NullPointerException("Решение не относится к вещественным числам");
+            }
+        }
+    }
+}
